c18663b0eb239a652fd188e08efdf9fc60d98310
Happysttim <gmlsdyd1023@naver.com>
2023-12-18 14:56:31 +0900

    Banner Navigator 오류 수정 및 자잘한거 수정함

--- obj/Debug/net8.0/NowonMedical.AssemblyInfo.cs
+++ obj/Debug/net8.0/NowonMedical.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NowonMedical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32fb3fde6ddaf282fe313b7d2f3519ad420fd34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec782c96341bc232633396fce1b536b8e23beea")]
 [assembly: System.Reflection.AssemblyProductAttribute("NowonMedical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NowonMedical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
